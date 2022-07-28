9b1b7440bf78f9dd4c6677ccc5f6f9e8f6942be7
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-07-28 20:23:35 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return data.employees.filter((pessoa) => (pessoa.firstName === employeeName
+      || pessoa.lastName === employeeName)).shift() || {};
 }
 
 module.exports = getEmployeeByName;

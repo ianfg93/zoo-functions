915d18db0a61b5ceeb15d5cc0f9b7c089d383e66
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-07-28 16:02:42 -0300

    requisito 6/7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,26 @@
+/* eslint-disable no-undef */
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se vazio retornar undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Verifica quantidade de Elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Verifica os nomes dos Elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Verifica se idade média dos Elefantes é algo próximo de 10.5', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Verifica a localização dos Elefantes', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Verifica a popularity de Elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Verifica dias de vistas dos Elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});

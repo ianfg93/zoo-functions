664cfc0aa1f974fd594e6b83a03af9abf3515719
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-07-28 18:27:17 -0300

    Requisito  8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrada) => entrada.age < 18);
+  const adult = entrants.filter((entrada) => entrada.age >= 18 && entrada.age < 50);
+  const senior = entrants.filter((entrada) => entrada.age >= 50);
+  return { child: child.length, adult: adult.length, senior: senior.length };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const publico = countEntrants(entrants);
+  const childValor = publico.child * data.prices.child;
+  const adultValor = publico.adult * data.prices.adult;
+  const seniorValor = publico.senior * data.prices.senior;
+  return childValor + adultValor + seniorValor;
 }
-
 module.exports = { calculateEntry, countEntrants };

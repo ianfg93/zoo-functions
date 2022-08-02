3a6741953028452798205028ea0306f1985ec52e
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-08-02 20:39:18 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const acharFunc = (valor) => {
+  const nome = data.employees.find(
+    (nomes) => nomes.firstName === valor.name || nomes.lastName === valor.name
+     || nomes.id === valor.id,
+  );
+  if (!nome) {
+    throw new Error('Informações inválidas');
+  }
+  const { id, firstName, lastName, responsibleFor } = nome;
+  const fullName = `${firstName} ${lastName}`;
+  const locations = [];
+  const animais = responsibleFor.map((a) => {
+    const bicho = data.species.find((especie) => especie.id === a);
+    locations.push(bicho.location);
+    return bicho.name;
+  });
+  return { id, fullName, species: animais, locations };
+};
+
+const funcionarios = () => {
+  const todosFuncionarios = [];
+  data.employees.forEach((func) => todosFuncionarios.push(acharFunc(func)));
+  return todosFuncionarios;
+};
+
+function getEmployeesCoverage(objeto) {
+  if (!objeto) {
+    return funcionarios();
+  }
+  return acharFunc(objeto);
 }
 
 module.exports = getEmployeesCoverage;

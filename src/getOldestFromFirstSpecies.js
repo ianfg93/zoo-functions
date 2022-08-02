1b4bd8444558d882d1c1be18a68fa84ad03bb613
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-08-02 10:47:51 -0300

    requisito 12/13

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colabResp = data.employees.find((colaborador) => colaborador.id === id).responsibleFor[0];
+  const selectEspecie = data.species.find((especie) => especie.id === colabResp).residents;
+  const animalVelho = selectEspecie.sort((idadeA, idadeB) => idadeA.age - idadeB.age)[0];
+  return Object.values(animalVelho);
 }
 
 module.exports = getOldestFromFirstSpecies;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,33 +1,19 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-// describe('Testes da função getOpeningHours', () => {
-//   it('Verifica se vazio retornar undefined', () => {
-//     expect(getOpeningHours()).toBeUndefined();
-//   });
-//   it('Verifica funcionamento do Zoo', () => {
-//     expect(getOpeningHours('hours')).toEqual({
-//       Tuesday: { open: 8, close: 6 },
-//       Wednesday: { open: 8, close: 6 },
-//       Thursday: { open: 10, close: 8 },
-//       Friday: { open: 10, close: 8 },
-//       Saturday: { open: 8, close: 10 },
-//       Sunday: { open: 8, close: 8 },
-//       Monday: { open: 0, close: 0 },
-//     });
-//   });
-//   it('Verifica se segunda-Feira está fechado', () => {
-//     expect(getOpeningHours('Monday')).toEqual('The zoo is closed');
-//   });
-//   it('Verifica se AM / PM são válidos', () => {
-//     expect(getOpeningHours('validateAbbreviation')).toBe('AM, PM');
-//   });
-//   it('Verifica horário de funcionamentode Quarta-Feira ', () => {
-//     expect(getOpeningHours('Wednesday')).toEqual({ open: 8, close: 6 });
-//   });
-//   it('Verifica horário de funcionamentode Sábado ', () => {
-//     expect(getOpeningHours('Saturday')).toEqual({ open: 8, close: 10 });
-//   });
-//   it('Verifica horário de funcionamento Domingo', () => {
-//     expect(getOpeningHours('Sunday')).toEqual({ open: 8, close: 8 });
-//   });
-// });
+describe('Testes da função getOpeningHours', () => {
+  it('Verificar tipo de parâmetro', () => {
+    expect(typeof getOpeningHours('Monday', '09:00-AM')).toEqual('string');
+  });
+  it('Verifica erros nos parâmetros', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrowError('The day must be valid. Example: Monday');
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrowError('The abbreviation must be \'AM\' or \'PM\'');
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrowError('The hour should represent a number');
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrowError('The minutes should represent a number');
+  });
+  it('Verifica funcionamento do Zoo', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual('The zoo is open');
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrowError('The day must be valid. Example: Monday');
+  });
+});

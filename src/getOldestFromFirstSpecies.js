74e8fc855658fcc04342ffaac1c213c1799b8b42
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-08-02 16:06:16 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,9 +2,11 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const colabResp = data.employees.find((colaborador) => colaborador.id === id).responsibleFor[0];
-  const selectEspecie = data.species.find((especie) => especie.id === colabResp).residents;
-  const animalVelho = selectEspecie.sort((idadeA, idadeB) => idadeA.age - idadeB.age)[0];
-  return Object.values(animalVelho);
+  const { residents } = data.species.find((especie) => especie.id === colabResp);
+  const animalVelho = residents.sort((idadeA, idadeB) => idadeA.age - idadeB.age);
+  const resultado = animalVelho[animalVelho.length - 1];
+  const { name, sex, age } = resultado;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
